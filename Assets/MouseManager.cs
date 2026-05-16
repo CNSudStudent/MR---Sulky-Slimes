fb744b8787d8b6cedbaa06146f6410d4b53a459f
cnsudstudent1 <cnsudstudent1@gmail.com>
2026-05-16 14:30:11 -0400

    I'm on step 33 of Sulky Slimes

--- Assets/MouseManager.cs
+++ Assets/MouseManager.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MouseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Mouse Info")]
+    public Vector3 clickStartLocation;
+
+    [Header("Physics")]
+    public Vector3 launchVector;
+    public float launchForce;
+
+    [Header("Slime")]
+    public Transform slimeTransform;
+    public Rigidbody slimeRigidbody;
+
+    [Header("Reset")]
+    public Vector3 originalSlimePosition;
+    public Quaternion originalRotationPosition;
+
     void Start()
     {
-        
+        originalSlimePosition = slimeTransform.position;
+        originalRotationPosition = slimeTransform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            slimeRigidbody.isKinematic = true;
+            slimeTransform.position = originalSlimePosition;
+            slimeTransform.rotation = originalRotationPosition;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickStartLocation = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mouseDifference = clickStartLocation - Input.mousePosition;
+            launchVector = new Vector3(
+                mouseDifference.x * 1f,
+                mouseDifference.y * 1.2f,
+                mouseDifference.y * 1.5f
+            );
+            slimeTransform.position = originalSlimePosition - launchVector / 400;
+            launchVector.Normalize();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            slimeRigidbody.isKinematic = false;
+            slimeRigidbody.AddForce(launchVector * launchForce, ForceMode.Impulse);
+        }
     }
 }
